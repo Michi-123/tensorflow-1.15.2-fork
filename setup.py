53289b562c99e1c9068fb1fe89f48d38af6f3d58
Michiyasu Uchiyama <hairstyle2004@yahoo.co.jp>
2023-05-02 17:42:18 +0900

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import find_packages
+from setuptools import setup
+from codecs import open
+import os
+
+current_path = os.getcwd()
+
+with open(os.path.join(current_path, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+
+setup(
+    name='tensorflow-fork'
+    packages=find_packages(exclude=('*.pyc',)),
+    version='1.15.2.0'
+    license='MIT', 
+    install_requires=[],
+    author='M.Uchiyama',
+    description='tensorflow_1.15.2', 
+    long_description=long_description,
+    long_description_content_type='text/markdown', 
+    keywords='TensorFlowFork', 
+
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.7',
+    ],
+)
